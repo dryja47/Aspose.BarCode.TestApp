88095bfb3a1c2968b12f989c08dfdbd9b3932575
KaizeR <murmaider47@gmail.com>
2021-12-28 22:03:42 +0500

    Initial commit

---
+++ Aspose.BarCode.TestApp/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Aspose.BarCode.BarCodeRecognition;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Aspose.BarCode.TestApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public JsonResult OnPost([FromForm] List<IFormFile> picture)
+        {
+            if (picture == null || picture.Count == 0) return new JsonResult(new List<BarCodeResult>());
+            using var stream = new MemoryStream();
+            picture.First().CopyTo(stream);
+            using var barCodeReader = new BarCodeReader(stream);
+            barCodeReader.ReadBarCodes();
+            return new JsonResult(barCodeReader.FoundBarCodes);
+        }
+    }
+}   
---
+++ Aspose.BarCode.TestApp/README.md
@@ -0,0 +1,10 @@
+﻿# Aspose.BarCode Test Web Application
+
+This single page application uses Aspose.BarCode package to recognize bar codes from user image.
+User uploads an image via form input. When the "go" button is pressed, the image is shown on the page.
+Ajax request with the image is sent to backend asynchronously. While the request is being processed, progress bar is shown. The picture is transferred to the BarCodeReader using a memory stream in the backend.
+After the codes are recognized on the frontend, the text values of the codes are displayed, and the codes themselves are highlighted with rectangles over the picture (by creating new divs).
+
+## Ideas for future improvements
+
+Using canvas instead of substituting the image blob into the src attribute of the img tag. So the image and rectangle highlighting will draw directly on canvas.
